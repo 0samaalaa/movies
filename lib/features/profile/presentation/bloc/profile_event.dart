cc0bf573bf31ce9fd956788976390c632795fb58
0samaalaa <usama.alaa7@gmail.com>
2025-09-11 03:30:42 +0300

    profile ui and logic updating

--- lib/features/profile/presentation/bloc/profile_event.dart
+++
@@ -1,9 +0,0 @@
-abstract class ProfileEvent {}
-
-class ResetPasswordEvent extends ProfileEvent {
-  final String oldPassword;
-  final String newPassword;
-
-  ResetPasswordEvent({required this.oldPassword, required this.newPassword});
-}
-
